7a4dd0155d828e6f64541caa587c3e9a42054306
kadoshn3 <neevk0622@gmail.com>
2023-11-13 23:29:59 -0500

    Added string replace question

---
+++ cracking_the_coding_interview/1_3_urlify.py
@@ -0,0 +1,10 @@
+#!/Users/neevekadosh/anaconda3/bin/python3
+
+def urlify(str1):
+    return str1.replace(" ", "%20")
+
+if __name__=='__main__':
+    # Replace spaces with %20 - should all return True
+    print("hello%20" == urlify("hello "))
+    print("hello%20world" == urlify("hello world"))
+    print("hello%20%20world%20" == urlify("hello  world "))
